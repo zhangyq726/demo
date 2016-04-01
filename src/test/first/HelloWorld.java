a27e2804a67b755dfc0ddd3956800a642ddb6494
zhangyq726 <zhangyq726@gmail.com>
2016-04-01 22:51:33 +0800

    push 1

--- src/test/first/HelloWorld.java
+++ src/test/first/HelloWorld.java
@@ -2,6 +2,8 @@ package test.first;
 
 public class HelloWorld {
 
+	private String name = "zhangyq";
+			
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
